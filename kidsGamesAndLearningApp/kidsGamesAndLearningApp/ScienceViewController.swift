702ed297287b14369cff5afecdaca99499dc8806
memaynor <mr.maynor20@gmail.com>
2023-04-18 10:22:46 -0500

    Science Controller Made

---
+++ kidsGamesAndLearningApp/kidsGamesAndLearningApp/ScienceViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ScienceViewController.swift
+//  kidsGamesAndLearningApp
+//
+//  Created by student on 4/18/23.
+//
+
+// Max Science
+
+import UIKit
+
+class ScienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
