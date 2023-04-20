382b2e0e173cc18d9183802565d0c39f006cd4a5
Parker W Christy <pchristy03@gmail.com>
2023-04-20 10:38:18 -0500

    add question marks

--- kidsGamesAndLearningApp/kidsGamesAndLearningApp/MatchingViewController.swift
+++ kidsGamesAndLearningApp/kidsGamesAndLearningApp/MatchingViewController.swift
@@ -25,6 +25,8 @@ class MatchingViewController: UIViewController {
     var tiles = [Tile]()
     var tileLocations = [UIImageView]()
     var gest = [UITapGestureRecognizer]()
+    var images = [String]()
+    var found = [Tile]()
     
     var initialImage = "questionmark"
     
@@ -33,7 +35,7 @@ class MatchingViewController: UIViewController {
     let tileB = Tile(image: "bird", id: 7, selected: false)
     
     let tileC = Tile(image: "cat", id: 6, selected: false)
-
+    
     let tileD = Tile(image: "rabbit", id: 5, selected: false)
     
     let tileE = Tile(image: "dog", id: 4, selected: false)
@@ -41,7 +43,7 @@ class MatchingViewController: UIViewController {
     let tileF = Tile(image: "mouse", id: 3, selected: false)
     
     let tileG = Tile(image: "horse", id: 2, selected: false)
-
+    
     let tileH = Tile(image: "sheep", id: 1, selected: false)
     
     let tileI = Tile(image: "cow", id: 9, selected: false)
@@ -49,7 +51,7 @@ class MatchingViewController: UIViewController {
     let tileJ = Tile(image: "bird", id: 10, selected: false)
     
     let tileK = Tile(image: "cat", id: 11, selected: false)
-
+    
     let tileL = Tile(image: "rabbit", id: 12, selected: false)
     
     let tileM = Tile(image: "dog", id: 13, selected: false)
@@ -57,7 +59,7 @@ class MatchingViewController: UIViewController {
     let tileN = Tile(image: "mouse", id: 14, selected: false)
     
     let tileO = Tile(image: "horse", id: 15, selected: false)
-
+    
     let tileP = Tile(image: "sheep", id: 16, selected: false)
     
     
@@ -81,37 +83,79 @@ class MatchingViewController: UIViewController {
     
     
     func randomTileLocation(){
-        var i = 0
+        var index = 0
         tiles.shuffle()
+        for tile in tiles {
+            images.append(tile.image)
+        }
+        
         for tileL in tileLocations {
-            tileL.image = UIImage(named: tiles[i].image)
-            i = i + 1;
+            tileL.image = UIImage(named: initialImage)
+            index = index + 1
+        }
+    }
+    
+    func exposeIfTapped(t: UIImageView) {
+        var index = 0
+        for tile in tiles{
+            if tile.id == Int(t.restorationIdentifier!) ?? 0{
+                tile.selected = true
+                tileLocations[index].image = UIImage(named: tile.image)
+            }
+            index = index + 1
+        }
+    }
+    
+    func resetImages(taps: Int, to: Int){
+        var i = 0
+        if taps == 3 && total != 17 {
+            for t in tiles {
+                if t.selected {
+                    tileLocations[i].image = UIImage(named: initialImage)
+                }
+                i = i + 1
+            }
         }
     }
     
     var selectedList = [UIImageView]()
     var total = 0
+    var taps = 0
+    var counter = 0
     
     @objc func imageTapped(gesture: UIGestureRecognizer) {
         // if the tapped view is a UIImageView then set it to imageview
         
         var count = 0
         
+        
         for t in tileLocations {
             t.restorationIdentifier = String(tiles[count].id)
             count = count + 1
         }
         
-            if let tileView = gesture.view as? UIImageView {
-                print(tileView.restorationIdentifier!)
-                selectedList.append(tileView)
-                    total = total + Int(tileView.restorationIdentifier!)!
-                    if total == 17 {
-                        print("match")
-                        total = 0
-                    }
-                }
+        if let tileView = gesture.view as? UIImageView {
+            //print(tileView.restorationIdentifier!)
+            selectedList.append(tileView)
+            print(tileView.restorationIdentifier)
+            print(selectedList.count)
+            total = total + Int(tileView.restorationIdentifier!)!
+            exposeIfTapped(t: tileView)
+            if total == 17 {
+                print("match")
+                total = 0
             }
+            //print(taps)
+            //print(total)
+            taps = taps + 1
+            if taps > 2 {
+                resetImages(taps: taps, to: total)
+                taps = 0
+                total = 0
+                selectedList.removeAll()
+            }
+        }
+    }
     
     
     override func viewDidLoad() {
@@ -120,13 +164,13 @@ class MatchingViewController: UIViewController {
         
         tiles = [tileA, tileB, tileC, tileD,
                  tileE, tileF, tileG, tileH,
-                tileI, tileJ, tileK, tileL,
-                tileM, tileN, tileO, tileP]
+                 tileI, tileJ, tileK, tileL,
+                 tileM, tileN, tileO, tileP]
         
         tileLocations = [tileOne, tileTwo, tileThree, tileFour, tileFive, tileSix,
                          tileSeven, tileEight, tileNine, tileTen, tileEleven,
                          tileTwelve, tileThirteen, tileFourteen, tileFifteen, tileSixteen]
-    
+        
         randomTileLocation()
         
         let tap1 = UITapGestureRecognizer(target: self, action: #selector(self.imageTapped(gesture:)))
@@ -153,20 +197,20 @@ class MatchingViewController: UIViewController {
             t.addGestureRecognizer(gest[i])
             i = i + 1;
         }
-
+        
     }
-
-
     
-
+    
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
