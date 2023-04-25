8e793994390d01a95aa8bcd135f28cf44bd64567
WesleyM2874 <79490347+WesleyM2874@users.noreply.github.com>
2023-04-25 16:41:41 -0500

    Crossword Connections Finished WM

---
+++ kidsGamesAndLearningApp/kidsGamesAndLearningApp/CrosswordViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CrosswordViewController.swift
+//  kidsGamesAndLearningApp
+//
+//  Created by Millett,Wesley M on 4/25/23.
+//
+
+import UIKit
+
+class CrosswordViewController: UIViewController {
+    @IBOutlet weak var matt1OL: UITextField!
+    @IBOutlet weak var moon4OL: UITextField!
+    @IBOutlet weak var moon2OL: UITextField!
+    @IBOutlet weak var moon1OL: UITextField!
+    @IBOutlet weak var math4OL: UITextField!
+    @IBOutlet weak var math3OL: UITextField!
+    @IBOutlet weak var math1OL: UITextField!
+    @IBOutlet weak var vol5OL: UITextField!
+    @IBOutlet weak var vol4OL: UITextField!
+    @IBOutlet weak var vol3OL: UITextField!
+    @IBOutlet weak var vol2OL: UITextField!
+    @IBOutlet weak var vol1OL: UITextField!
+    @IBOutlet weak var matt6OL: UITextField!
+    @IBOutlet weak var matt5OL: UITextField!
+    @IBOutlet weak var matt4OL: UITextField!
+    @IBOutlet weak var matt3OL: UITextField!
+    @IBOutlet weak var matt2OL: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
